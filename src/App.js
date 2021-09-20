56ac81d6dd49654ec1f8efabecd8bdda62c1cdb4
unknown <cafb.lagotronics.ph@gmail.com>
2021-09-20 08:30:33 +0800

    updated na ulitss

--- src/App.js
+++ src/App.js
@@ -154,7 +154,7 @@ function App() {
   })
   return (
   <div className="Background">
-    <globalItem.Provider value={{buttonData , user , setUser , setColor , auth, setAuth ,indicatorData,accessoriesData,audioData,breakerData,connectorsData,consumableData,contactorData,controllerData, counterData, disconnectSwitchData, enclosureData, ethernetData, lightData, motorAccesoryData, motorDriverData, motor }}>
+    <globalItem.Provider value={{buttonData , user , setUser , setColor , auth, setAuth ,indicatorData,accessoriesData,audioData,breakerData,connectorsData,consumableData,contactorData,controllerData, counterData, disconnectSwitchData, enclosureData, ethernetData, lightData, motorAccesoryData, motorDriverData, motorData, powerSupplyData, relayData, sensorData, switchData, tempControlData, timerData, toolsData, waterEffectsData, wireData }}>
       <Router>
         <Switch>    
           <Route exact path="/">
